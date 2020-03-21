8cdb477ddcff137c70510f5eff94602afd7cc505
Mark Grebe <grebe@ucmo.edu>
2020-03-20 20:41:43 -0500

    Correct Security config and add info page

--- src/main/java/edu/ucmo/spring_example/controller/HomeController.java
+++ src/main/java/edu/ucmo/spring_example/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping(value = "/info")
+    public String info() {
+        return "info";
+    }
 }
