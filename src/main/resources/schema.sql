fba60a75b4a478a6ac0c689fa7cc762689b3715f
Mark Grebe <grebe@ucmo.edu>
2020-04-30 16:31:05 -0500

    Fix Table Name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE car (
   year   INT         NOT NULL
 );
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
   user_id       SERIAL PRIMARY KEY,
   user_name     VARCHAR(50) NOT NULL,
   email         VARCHAR(50) NOT NULL,
