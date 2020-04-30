a8157c58e2d20e8cb3a928dc1b443f466dd157da
Mark Grebe <grebe@ucmo.edu>
2020-04-30 16:27:50 -0500

    Added more schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,20 @@ CREATE TABLE car (
   model  VARCHAR(50) NOT NULL,
   year   INT         NOT NULL
 );
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  user_id       SERIAL PRIMARY KEY,
+  user_name     VARCHAR(50) NOT NULL,
+  email         VARCHAR(50) NOT NULL,
+  password      VARCHAR(50) NOT NULL,
+  first_name    VARCHAR(50) NOT NULL,
+  last_name     VARCHAR(50) NOT NULL,
+  active        BOOLEAN NOT NULL
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+  role_id   SERIAL PRIMARY KEY,
+  role      VARCHAR(50) NOT NULL
+);
